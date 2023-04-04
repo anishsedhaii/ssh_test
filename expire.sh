6483c6b8f58f6a28d1d241ee46482d182404e4c5
Anish Sedhai <30548961+anishsedhaii@users.noreply.github.com>
2023-04-04 12:57:08 +0545

    Create expire.sh

---
+++ expire.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+exdate=`echo | whois $1 | grep -E 'paid|Expiry|Expires' | grep -o -E '[0-9]{4}.[0-9]{2}.[0-9]{2}|[0-9]{2}/[0-9]{2}/[0-9]{4}'`
+expire=$((`date -d "$exdate" '+%s'`))
+today=$((`date '+%s'`))
+leftsec=$(($expire - $today))
+leftdays=$(($leftsec/86400))
+echo $leftdays
